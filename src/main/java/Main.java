987fb15fe11f60977470dba99d19e1035b6ed1db
GwenDarrot <gwendoline.darrot@gmail.com>
2025-04-15 16:48:07 +0200

    Creacion de la clase principal para inicializaciones o pruebas.

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Clase principal para iniciar la aplicación.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Bienvenido al sistema de Gestión de Turnos y Solicitudes del Personal Hospitalario");
+        // Se puede invocar la lógica de servicios para pruebas o inicializaciones
+    }
+}
