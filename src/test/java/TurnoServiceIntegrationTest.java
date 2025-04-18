7466ba98d88d5c1b07a6c3981c70c255e4d1514c
GwenDarrot <gwendoline.darrot@gmail.com>
2025-04-18 02:59:16 +0200

    Creo la clase TurnoServiceIntegrationTest para realizar las pruebas de integración con Mockito

---
+++ src/test/java/TurnoServiceIntegrationTest.java
@@ -0,0 +1,17 @@
+import static org.mockito.Mockito.*;
+
+import model.Turno;
+import org.junit.jupiter.api.Test;
+import repository.TurnoRepository;
+import service.TurnoService;
+
+public class TurnoServiceIntegrationTest {
+
+    @Test
+    public void testAsignarTurnoConRepositorioMock() {
+
+    }
+
+    private void assertTrue(boolean resultado) {
+    }
+}
